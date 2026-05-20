7bd0050e226f6fc4c985a061ba57d9ed9ef1ad6c
jujuneos <lcjuniorconceicao@gmail.com>
2026-05-20 18:02:55 -0300

    Cria controller e adiciona data de nascimento na edição

--- Application/UseCases/Usuarios/Commands/EditarUsuarioCommand.cs
+++ Application/UseCases/Usuarios/Commands/EditarUsuarioCommand.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Application.UseCases.Usuarios.Commands;
 
-public record EditarUsuarioCommand(int Id, string Nome, string Email) : IRequest<Result>
+public record EditarUsuarioCommand(int Id, string Nome, string Email, DateTime dataNascimento) : IRequest<Result>
 {
     public class Validator : AbstractValidator<EditarUsuarioCommand>
     {
@@ -25,6 +25,8 @@ public record EditarUsuarioCommand(int Id, string Nome, string Email) : IRequest
             RuleFor(x => x.Email)
                 .NotEmpty().WithMessage("O email é obrigatório.")
                 .EmailAddress().WithMessage("O email deve ser válido.");
+            RuleFor(x => x.dataNascimento)
+                .LessThan(DateTime.UtcNow).WithMessage("A data de nascimento deve ser no passado.");
         }
     }
 };
@@ -52,6 +54,7 @@ public class EditarUsuarioCommandHandler : IRequestHandler<EditarUsuarioCommand,
 
         usuario.Nome = request.Nome;
         usuario.Email = request.Email;
+        usuario.DataNascimento = request.dataNascimento;
         usuario.DataEdicao = DateTime.UtcNow;
 
         try
---
+++ TesteDotNet/Controllers/UsersController.cs
@@ -0,0 +1,100 @@
+﻿using Application.UseCases.Usuarios.Commands;
+using Application.UseCases.Usuarios.Queries;
+using Domain.Entidades;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TesteDotNet.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly ISender sender;
+
+    public UsersController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    /// <summary>
+    /// Retorna uma lista paginada de todos os usuários cadastrados.
+    /// </summary>
+    /// <param name="pagina">Número da página.</param>
+    /// <returns>Lista de usuários.</returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAll([FromQuery] int pagina = 1)
+    {
+        var usuarios = await sender.Send(new ObterTodosOsUsuariosQuery(pagina));
+        return Ok(usuarios);
+    }
+
+    /// <summary>
+    /// Obtém um usuário pelo ID fornecido.
+    /// Se o usuário não for encontrado, uma mensagem de erro será retornada.
+    /// </summary>
+    /// <param name="id">Identificador do usuário.</param>
+    /// <returns>O usuário encontrado.</returns>
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var usuario = await sender.Send(new ObterUsuarioPorIdQuery(id));
+        return Ok(usuario);
+    }
+
+    /// <summary>
+    /// Cria usuário com os dados fornecidos.
+    /// O nome deve conter no máximo 50 caracteres e o e-mail deve ser válido.
+    /// Se um usuário já existir com o mesmo e-mail, uma mensagem de erro será retornada.
+    /// </summary>
+    /// <param name="nome">Nome do usuário.</param>
+    /// <param name="email">E-mail do usuário.</param>
+    /// <param name="dataNascimento">Data de nascimento do usuário.</param>
+    /// <returns>O ID do usuário criado.</returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromBody] string nome, [FromBody] string email, [FromBody] DateTime dataNascimento)
+    {
+        var result = await sender.Send(new CriarUsuarioCommand(nome, email, dataNascimento));
+        return Ok(result.Value);
+    }
+
+    /// <summary>
+    /// Atualiza os dados de um usuário existente com base no ID fornecido.
+    /// Caso o usuário não seja encontrado, uma mensagem de erro será retornada.
+    /// </summary>
+    /// <param name="id">Identificador do usuário.</param>
+    /// <param name="nome">Nome do usuário.</param>
+    /// <param name="email">E-mail do usuário.</param>
+    /// <param name="dataNascimento">Data de nascimento do usuário.</param>
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] string nome, [FromBody] string email, [FromBody] DateTime dataNascimento)
+    {
+        var result = await sender.Send(new EditarUsuarioCommand(id, nome, email, dataNascimento));
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Deleta um usuário existente com base no ID fornecido.
+    /// Caso o usuário não seja encontrado, uma mensagem de erro será retornada.
+    /// </summary>
+    /// <param name="id">Identificador do usuário.</param>
+    /// <returns>O ID do usuário deletado.</returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(Usuario), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        var result = await sender.Send(new DeletarUsuarioCommand(id));
+        return Ok(result);
+    }
+};
